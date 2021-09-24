efc317ad09f3f5de8bdca077cbb9644cde7eef9c
sm970309 <sm970309@gmail.com>
2021-09-24 16:52:46 +0900

    Add New File[7_bs4_webtoons.py]

---
+++ WEB_SCRAPING/7_bs4_webtoons.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://comic.naver.com/webtoon/weekday"
+res = requests.get(url)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text,"lxml")   # soup 객체로 만들기
+cartoons = soup.find_all("a",attrs={"class":"title"})
+
+for cartoon in cartoons:
+    print(cartoon.get_text())
