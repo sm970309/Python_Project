eaeea05d88d9d9c280e6e4405e3ad739dba4b8c3
sm970309 <sm970309@gmail.com>
2021-10-06 16:18:17 +0900

    Update File[16_selenium_movies_scroll.py]

--- WEB_SCRAPING/16_selenium_movies_scroll.py
+++ WEB_SCRAPING/16_selenium_movies_scroll.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
 import time
 
 browser = webdriver.Chrome()
@@ -23,3 +25,22 @@ while True:
     prev_height = curr_height
 
 print("스크롤 완료")
+
+soup = BeautifulSoup(browser.page_source,"lxml")
+
+movies = soup.find_all("div",attrs={"class":"Vpfmgd"})    # 리스트 형태-> , 사용해서 or 조건
+
+for movie in movies:
+    title = movie.find("div",attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    original_price = movie.find("span",attrs={"class":"SUZt4c djCuy"})
+    if original_price:
+        original_price = original_price.get_text()
+    else:
+        continue
+    price = movie.find("span",attrs={"class":"VfPpfd ZdBevf i5DZme"}).get_text()
+    link = movie.find("a",attrs={"class":"JC71ub"})["href"]
+    link = "https://play.google.com"+link
+
+    print(f"제목: {title}")
+    print(f"할인 전 금액{original_price}, 할인 후 금액{price}")
+    print(f"링크: {link}")
